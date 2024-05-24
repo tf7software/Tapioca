3234eb74767782d0b148029a848d4241ceb9255b
Henry Warren <contact@twentyfour7.xyz>
2024-05-23 17:38:54 -0700

    dasjdah

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3000;
 require('dotenv').config();
 
 app.get("/:universalURL", (req, res) => {
-   res.send("404 URL NOT FOUND");
+   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 // Middleware to parse JSON
